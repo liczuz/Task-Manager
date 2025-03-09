2211ea21014bead62a1fe78502b9824ae764b8c2
liczuz <zuzana.lichtenbergova@t-mobile.cz>
2025-03-09 13:46:26 +0100

    Add files via upload

--- projekt_01_test.py
+++ projekt_01_test.py
@@ -8,7 +8,7 @@ def hlavni_menu():
         print("3. Odstranit úkol")
         print("4. Konec programu")
         
-        volba = input("Vyberte možnost (1-4): ")
+        volba = input("Vyber možnost (1-4): ")
         
         if volba == "1":
             pridat_ukol()
@@ -18,22 +18,25 @@ def hlavni_menu():
             odstranit_ukol()
         elif volba == "4":
             print("Konec programu")
+            exit()
         else:
-            print("Neplatná volba, zkuste to znovu.")
+            print("Neplatná volba, zkus to znovu.")
 
 def pridat_ukol():
-    novy_ukol = input("Přidat nový úkol: ")
-    if novy_ukol:
-        ukoly.append(novy_ukol)
+    nazev = input("Zadej název úkolu: ")
+    popis = input("Zadej popis úkolu: ")
+    
+    if nazev and popis:
+        ukoly.append({"název": nazev, "popis": popis})
         print("Úkol byl přidán.")
     else:
-        print("Úkol nemůze být prázdný.")
+        print("Název a popis úkolu nemůžou být prázdné.")
 
 def zobrazit_ukoly():
     if ukoly:
         print("\nSeznam úkolů:")
         for index, ukol in enumerate(ukoly, start=1):
-            print(f"{index}. {ukol}")
+            print(f"{index}. {ukol['název']} - {ukol['popis']}")
     else:
         print("\nŽádný úkol nebyl přidán.")
 
@@ -44,14 +47,14 @@ def odstranit_ukol():
     
     zobrazit_ukoly()
     try:
-        cislo_ukolu = int(input("Zadejte číslo úkolu, který chcete odstranit: "))
+        cislo_ukolu = int(input("Zadej číslo úkolu, který chceš odstranit: "))
         if 1 <= cislo_ukolu <= len(ukoly):
             odstraneny_ukol = ukoly.pop(cislo_ukolu - 1)
-            print(f"Úkol '{odstraneny_ukol}' byl odstraněn.")
+            print(f"Úkol '{odstraneny_ukol ['název']}' byl odstraněn.")
         else:
             print("Neplatné číslo úkolu.")
     except ValueError:
-        print("Prosím zadejte platné číslo.")
+        print("Prosím zadej platné číslo.")
         
 if __name__ == "__main__":
     hlavni_menu()
